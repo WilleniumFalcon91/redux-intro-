9fffda4555e1b13b6b2ead6842b8565b76b9763f
William Elliott <wcelliott1991@gmail.com>
2017-09-08 15:06:58 -0400

    uodates store dispathes to the dom

--- main.js
+++ main.js
@@ -1,17 +1,28 @@
-  const counter = (state, action) => {
-        // if (!state)
-        if (typeof state === 'undefined') {
-            return 0;
-        }
-        
-        switch (action.type) {
-            case 'INCREMENT':
-                return state + 1
-            case "DECREMENT": 
-                return state -1;
-            default: 
-                return state;
+const counter = (state, action) => {
+    // if (!state)
+    if (typeof state === 'undefined') {
+        return 0;
+    }
+    
+    switch (action.type) {
+        case 'INCREMENT':
+            return state + 1
+        case "DECREMENT": 
+            return state -1;
+        default: 
+            return state;
 
-        }
-        
-        };
+    }
+    
+};
+
+const store = Redux.createStore(counter);
+const valueEl = document.querySelector('#value');
+const render = () => {
+  // console.log(store.getState());
+  valueEl.innerHTML = store.getState().toString();
+  
+}
+render();
+store.subscribe(render);
+
